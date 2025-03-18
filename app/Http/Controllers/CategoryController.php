2476533504f1c95316e4e61b463f20b3c3130d8d
aymane-06 <aymane.himame@gmail.Com>
2025-03-18 14:25:30 +0000

    Add initial project structure with configuration files, routes, models, and seeders

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $categories=Category::all();
+
+        return response()->json([
+            'message' => 'Success',
+            'data' => $categories
+        ], 200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCategoryRequest $request)
+    {
+        $request->validated([
+            'name' => 'required|string'
+        ]);
+        $category = Category::create($request->all());
+        return response()->json([
+            'message' => 'Category created successfully',
+            'data' => $category
+        ], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Category $category)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Category $category)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCategoryRequest $request, Category $category)
+    {
+        $request->validated([
+            'name' => 'required|string'
+        ]);
+        $category->update($request->all());
+        return response()->json([
+            'message' => 'Category updated successfully',
+            'data' => $category
+        ], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Category $category)
+    {
+        $category->delete();
+        return response()->json([
+            'message' => 'Category deleted successfully'
+        ], 200);
+    }
+}
